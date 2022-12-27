fa9d0f8d6ff07822bbdc8313dc1b3afc1b855d3d
BoooM0610 <eduardomartinezoc@gmail.com>
2022-12-27 11:58:54 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'exercice-1';
+
+  name!:string;
+
+  fonts = ['Verdana', 'Fantasy', 'Arial', 'Impact'];
+
+  font !: string;
+
+  font_size:number = 20;
+
+  font_align!: string;
+
+  setName (event:Event) {
+    this.name = ((<HTMLInputElement>event.target).value).toUpperCase();
+  }
+
+  setFont(event:Event) {
+    this.font = (<HTMLInputElement>event.target).value;
+    var element = document.getElementById('user_name');
+    if (element) {
+      element.style.fontFamily = this.font;
+    }
+  }
+
+  setSize (event:Event) {
+    this.font_size = parseInt((<HTMLInputElement>event.target).value);
+  }
+
+  setAlign(event:Event) {
+    this.font_align = (<HTMLInputElement>event.target).value;
+    var element = document.getElementById('user_name');
+    if (element) {
+      element.style.textAlign = this.font_align;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule
